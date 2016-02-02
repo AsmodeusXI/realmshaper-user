5c0f86a5521e4f283b55d75ea9ac94d42cf62d59
AsmodeusXI <AsmodeusXI@gmail.com>
2016-02-01 21:31:42 -0600

    init: begin realmshaper-user Express app
    Add package.json for NPM
    Add .travis.yml for Travis usage (future)
    Add server.js with basic config file and mongoose
    Add config/default.json with basic startup parameters

---
+++ server.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = function () {
+    const express = require('express');
+    const bodyParser = require('body-parser');
+    const cors = require('cors');
+    const app = express();
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+    app.use(cors());
+
+    // Config Setup
+    const config = require('./config/default')[process.env.NODE_ENV || 'local'];
+
+    // Mongoose Setup
+    const mongoose = require('mongoose');
+    mongoose.Promise = require('bluebird');
+    mongoose.connect(config.dbUrl);
+
+    // File Location Setup
+    app.use('/', express.static(__dirname));
+
+    // Default routes
+    app.get('*', function(req, res) {
+        res.send(config.message);
+    });
+
+    // Server startup
+    app.listen(config.port);
+}
+
+module.exports();
